5b3c183d256d26ca0ee771709e27de659e29421b
unknown <leechaehyun@hanwha.com>
2024-08-30 16:48:50 +0900

    environ setting

---
+++ workspace/vueproject/src/components/Login.vue
@@ -0,0 +1,21 @@
+
+<template>
+    <div>
+      테스트 내용 : {{ msg }}
+    </div>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      msg: "hello! it's a test for vue component"
+    }
+  },
+  computed: {
+     
+  }
+}
+
+</script>
